e77313dc48beb067a18f536540a5f56b79fff930
Marcio <marcinho.lc@hotmail.com>
2025-03-30 18:02:27 -0300

    Criado produto detail e list.html

--- mgmodas/urls.py
+++ mgmodas/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('produto/', include('produto.urls')),
     path('admin/', admin.site.urls),
+
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Produto
+
 
 # Create your views here.
+
+def produto_list(request):
+    template_name='produto_list.html'
+    objects = Produto.objects.all()
+    context = {'object_list':objects }
+    return render(request, template_name, context)
+
+def produto_detail(request,pk):
+    template_name='produto_detail.html'
+    obj = Produto.objects.get(pk=pk)
+    context = {'object':obj }
+    return render(request, template_name, context)
