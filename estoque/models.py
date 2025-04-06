ffcdf43f764ed3e633e62c8ae435c7d90f874daa
Marcio <marcinho.lc@hotmail.com>
2025-04-06 07:12:03 -0300

    estoque entrada detail

--- estoque/models.py
+++ estoque/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+
 from core.models import TimeStampedModel
 from produto.models import Produto
 
@@ -21,14 +23,17 @@ class Estoque(TimeStampedModel):
         ordering = ['-created_at']
 
     def __str__(self):
-        return '{} - {} - {}'.format(self.pk, self.nf, self.created.strftime('%d/%m/%Y'))
+        return '{} - {} - {}'.format(self.pk, self.nf, self.created_at.strftime('%d/%m/%Y'))
+
+    def get_absolute_url(self):
+        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk': self.pk})
 
     def nf_formated(self):
-        return str(self.nf).zfill(10)
+        return str(self.nf).zfill(6)
 
 
 class EstoqueItens(models.Model):
-    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE)
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE, related_name='estoques')
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField()
     saldo = models.PositiveIntegerField()
